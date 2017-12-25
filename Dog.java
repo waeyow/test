15dfcb055eb87fe9c568489d97eae4f98b7c5597
waeyow <penaapril1@gmail.com>
2017-12-25 22:17:03 +0800

    added dog.java file

---
+++ Dog.java
@@ -0,0 +1,19 @@
+class Dog
+{
+	int age;
+
+	public Dog(int dogsAge)
+	{
+		age = dogsAge;
+
+	}
+	public void bark(){
+		System.out.println("Woof");
+
+	}
+	public static void main (String[] args)
+	{
+		Dog spike = new Dog(5);
+		spike.bark();
+	}
+}
